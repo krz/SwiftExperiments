54506f2916bb912ab84d9208280c167448bc9a90
Christoph <krz@users.noreply.github.com>
2024-04-20 11:32:30 +0200

    add Instafilter app

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Christoph on 17.04.24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
